37eb4a649a98c988319e046901a814169c03cce0
Jacob Durman <Jacob.Durman@gmail.com>
2021-12-13 17:41:51 -0600

    Added base functionality and hot key validation.

--- wk_anki_mode.js
+++ wk_anki_mode.js
@@ -23,6 +23,22 @@ window.ankimode = {};
     wkof.ready('document,Menu,Settings').then(setup);
 
     var settings;
+    var activated = false;
+    var answerShown = false;
+    var firstCorrectAnswer = "";
+    var secondNoTriggered = false;
+
+    // Save the original evaluator
+    var originalChecker = answerChecker.evaluate;
+
+    var checkerYes = function (itemType, correctValue) {
+        return { accurate: !0, passed: !0 };
+    }
+
+    var checkerNo = function (itemType, correctValue) {
+        return { accurate: !0, passed: 0 };
+    }
+
 
     function setup() {
         wkof.Menu.insert_script_link({ name: 'ankimode', submenu: 'Settings', title: 'Anki Mode', on_click: open_settings });
@@ -46,8 +62,8 @@ window.ankimode = {};
                     type: 'page', label: 'Hotkeys', content: {
                         grpHotkeys: {
                             type: 'group', label: 'Hotkeys', content: {
-                                correct_hotkey: { type: 'text', label: 'Marks answer correct', default: true, hover_tip: 'Use only a single digit number or letter.' },
-                                incorrect_hotkey: { type: 'text', label: 'Marks answer "incorrect"', default: true, hover_tip: 'Use only a single digit number or letter.' },
+                                correct_hotkey: { type: 'text', label: 'Marks answer correct', default: true, hover_tip: 'Use only a single digit number or letter.', validate: validateHotkey, on_change: function (name, value, config) { settings.correct_hotkey = value.toUpperCase(); } },
+                                incorrect_hotkey: { type: 'text', label: 'Marks answer "incorrect"', default: true, hover_tip: 'Use only a single digit number or letter.', validate: validateHotkey, on_change: function (name, value, config) { settings.incorrect_hotkey = value.toUpperCase(); } },
                             }
                         },
                     }
@@ -64,6 +80,17 @@ window.ankimode = {};
             }
         });
         dialog.open();
+    }
+
+    function validateHotkey(value, config) {
+        if ((/^[A-Za-z0-9]{1}$/).test(value)) {
+            return true;
+        } else {
+            return {
+                valid: false,
+                msg: 'Must be a letter or single digit number'
+            }
+        }
     }
 
     function settings_preopen(dialog) {
@@ -98,11 +125,67 @@ window.ankimode = {};
 
 
     function startup() {
+        if (window.doublecheck) {
+            $('head').append('<style>' + doubleCheckCssModification + '</style>');
+        } else {
+            $('head').append('<style>' + css + '</style>');
+        }
 
-        // Install the Anki Mode button.
+
+        // Add the Anki Mode button.
         $('head').append('<style>#anki-mode.anki-active {color:#ff0; opacity:1.0;}</style>');
         $('#summary-button').append('<a id="anki-mode" href="#"><i class="fa fa-star" title="Anki Mode - This allows you to turn on or off anki mode."></i></a>');
         $('#anki-mode').on('click', ankimode_clicked);
+
+        //Add the Correct, Incorrect, and Show Answer buttons
+        $("<div />", {
+            id: "WKANKIMODE_buttons"
+        })
+            .addClass("WKANKIMODE_buttons")
+            .prependTo("#additional-content");
+
+        $("<div />", {
+            id: "WKANKIMODE_anki_incorrect",
+            title: "Shortcut: L",
+        })
+            .text("Don't know")
+            .addClass("WKANKIMODE_button incorrect")
+            .on("click", answerIncorrect)
+            .prependTo("#WKANKIMODE_buttons");
+
+        $("<div />", {
+            id: "WKANKIMODE_anki_show",
+            title: "Shortcut: Space",
+        })
+            .text("Show Answer")
+            .addClass("WKANKIMODE_button show")
+            .on("click", showAnswer)
+            .prependTo("#WKANKIMODE_buttons");
+
+        $("<div />", {
+            id: "WKANKIMODE_anki_correct",
+            title: "Shortcut: K",
+        })
+            .text("Know")
+            .addClass("WKANKIMODE_button correct")
+            .on("click", answerCorrect)
+            .prependTo("#WKANKIMODE_buttons");
+
+        if (window.doublecheck) {
+            $('body').on('click', '#option-retype', function (event) {
+                if (activated) {
+                    $("#user-response").val('');
+                    answerShown = false;
+                    hideAnswerButtons();
+                }
+            });
+        }
+
+        //bind the hotkeys
+        bindHotkeys();
+
+        //Start ankimode events
+        settings.ankimode_enabled ? ankimode_start() : ankimode_stop();
 
     }
 
@@ -110,8 +193,270 @@ window.ankimode = {};
         settings.ankimode_enabled = !settings.ankimode_enabled;
         wkof.Settings.save('ankimode');
         $('#anki-mode').toggleClass('anki-active', settings.ankimode_enabled);
+
+        settings.ankimode_enabled ? ankimode_start() : ankimode_stop();
+
         return false;
     }
 
+    function ankimode_start() {
+        activated = true;
+        $('#user-response').focus(function (e) {
+            $(this).blur();
+        });
+        $('#user-response').blur();
+
+        //show anki mode buttons
+        $(".WKANKIMODE_button.correct").hide();
+        $(".WKANKIMODE_button.incorrect").hide();
+        $(".WKANKIMODE_button.show").show();
+    }
+
+    function ankimode_stop() {
+        activated = false;
+        //remove event listeners
+        $("#user-response").off("focus");
+
+        //hide anki mode buttons
+        $(".WKANKIMODE_button.correct").hide();
+        $(".WKANKIMODE_button.incorrect").hide();
+        $(".WKANKIMODE_button.show").hide();
+
+        $("#user-response").focus();
+
+        if (!$("#answer-form form fieldset").hasClass("correct") && !$("#answer-form form fieldset").hasClass("incorrect")) {
+            $("#user-response").val("");
+        }
+    }
+
+    function showAnswer() {
+        if (!$("#answer-form form fieldset").hasClass("correct") &&
+            !$("#answer-form form fieldset").hasClass("incorrect") &&
+            !answerShown) {
+            firstCorrectAnswer = "";
+            var currentItem = $.jStorage.get("currentItem");
+            var questionType = $.jStorage.get("questionType");
+            if (questionType === "meaning") {
+                var answer = currentItem.en.join(", ");
+                if (currentItem.syn.length) {
+                    answer += " (" + currentItem.syn.join(", ") + ")";
+                }
+                firstCorrectAnswer = currentItem.en[0];
+                $("#user-response").val(answer);
+            } else { //READING QUESTION
+                var i = 0;
+                var answer = "";
+                if (currentItem.voc) {
+                    answer += currentItem.kana[0];
+                } else if (currentItem.emph == 'kunyomi') {
+                    answer += currentItem.kun[0];
+                } else if (currentItem.emph == 'nanori') {
+                    answer += currentItem.nanori[0];
+                } else {
+                    answer += currentItem.on[0];
+                }
+                firstCorrectAnswer = answer;
+                $("#user-response").val(answer);
+            }
+            answerShown = true;
+            showAnswerButtons();
+        }
+    }
+
+    function answerCorrect() {
+        // Fix for multiple answers in reading
+        if (firstCorrectAnswer) {
+            $("#user-response").val(firstCorrectAnswer);
+            firstCorrectAnswer = "";
+        }
+
+        if (answerShown) {
+            answerChecker.evaluate = checkerYes;
+            $("#answer-form form button").click();
+            answerShown = false;
+            answerChecker.evaluate = originalChecker;
+            return;
+        }
+
+        // if answer is shown, press '1' one more time to go to next
+        if ($("#answer-form form fieldset").hasClass("correct") ||
+            $("#answer-form form fieldset").hasClass("incorrect")) {
+            $("#answer-form form button").click();
+            hideAnswerButtons();
+        }
+    }
+
+    function answerIncorrect() {
+        if (answerShown) {
+            //fix for doublecheck
+            if (window.doublecheck) {
+                var questionType = $.jStorage.get("questionType");
+                if (questionType === 'meaning') {
+                    $("#user-response").val('xxxxxx');
+                } else {
+                    $("#user-response").val('ばつっっっ');
+                }
+            }
+
+            answerChecker.evaluate = checkerNo;
+            $("#answer-form form button").click();
+            answerShown = false;
+            answerChecker.evaluate = originalChecker;
+            return;
+        }
+
+        if ($("#answer-form form fieldset").hasClass("correct") ||
+            $("#answer-form form fieldset").hasClass("incorrect")) {
+            if (window.doublecheck) {
+                if (!secondNoTriggered) {
+                    secondNoTriggered = true;
+                    setTimeout(function () {
+                        $("#answer-form form button").click();
+                        hideAnswerButtons();
+                        secondNoTriggered = false;
+                    }, settings.doublecheck_delay_period * 1000); //doublecheck default delay for wrong answers before you can submit
+                }
+            } else {
+                $("#answer-form form button").click();
+                hideAnswerButtons();
+            }
+        }
+    }
+
+    function hideAnswerButtons() {
+        $(".WKANKIMODE_button.correct").hide();
+        $(".WKANKIMODE_button.incorrect").hide();
+        $(".WKANKIMODE_button.show").show();
+    }
+
+    function showAnswerButtons() {
+        $(".WKANKIMODE_button.correct").show();
+        $(".WKANKIMODE_button.incorrect").show();
+        $(".WKANKIMODE_button.show").hide();
+    }
+
+    function bindHotkeys() {
+        $('body').on("keydown", function (event) {
+
+            if ($("#reviews").is(":visible") && !$("*:focus").is("textarea, input")) {
+                switch (event.keyCode) {
+                    //key: enter
+                    case 13:
+                        if ($("#answer-form form fieldset").hasClass("correct") ||
+                            $("#answer-form form fieldset").hasClass("incorrect")) {
+                            hideAnswerButtons();
+                        }
+                        return;
+                        break;
+                    //key: Space
+                    case 32:
+                        event.stopPropagation();
+                        event.preventDefault();
+
+                        if (activated)
+                            showAnswer();
+
+                        return;
+                        break;
+                    case 27: //key: escape (only needed when doublecheck is active)
+                        if (activated) {
+                            if (window.doublecheck) {
+                                $("#user-response").val('');
+                                answerShown = false;
+                                hideAnswerButtons();
+                            }
+
+                        }
+
+                        return;
+                        break;
+                    case 8: //key: backspace (only needed when doublecheck is active)
+                        if (activated) {
+                            if (window.doublecheck) {
+                                $("#user-response").val('');
+                                answerShown = false;
+                                hideAnswerButtons();
+                            }
+                        }
+                        return;
+                        break;
+                    default:
+                        if (settings.correct_hotkey.charCodeAt(0) == event.keyCode) {
+                            event.stopPropagation();
+                            event.preventDefault();
+
+                            if (activated)
+                                answerCorrect();
+
+                            return;
+                            break;
+                        } else if (settings.incorrect_hotkey.charCodeAt(0) == event.keyCode) {
+                            event.stopPropagation();
+                            event.preventDefault();
+
+                            if (activated)
+                                answerIncorrect();
+
+                            return;
+                            break;
+                        }
+
+                        return;
+                        break;
+                }
+            }
+        });
+    };
+
+    var css = "\
+#WKANKIMODE_anki { \
+    background-color: #e1e1e1; \
+    color: #3c3c3c; \
+    margin: 0 5px; \
+    width: auto; \
+    padding: 6px; \
+} \
+#WKANKIMODE_yes { \
+    background-color: #009900; \
+    margin: 0 0 0 5px; \
+} \
+#WKANKIMODE_no { \
+    background-color: #990000; \
+} \
+.WKANKIMODE_button { \
+    width: 50%; \
+    display: inline-block; \
+    text-align:center; \
+    font-size: 0.8125em; \
+    color: #FFFFFF; \
+    cursor: pointer; \
+    padding: 10px 0; \
+    margin-bottom: 5px; \
+    border: 1px solid transparent \
+} \
+ .WKANKIMODE_buttons { \
+    display: flex; \
+    position: relative; \
+    width: 100%; \
+} \
+.WKANKIMODE_buttons .incorrect { \
+    background-color: #f03; \
+} \
+.WKANKIMODE_buttons .correct { \
+    background-color: #88cc00; \
+} \
+.WKANKIMODE_buttons .show { \
+background-color: #0af; \
+width:100%;\
+} \
+#WKANKIMODE_anki.hidden { \
+display: none; \
+} ";
+
+
+    var doubleCheckCssModification = css + "\
+#answer-exception { \
+top:5.9em \
+} ";
 })(window.ankimode);
 
